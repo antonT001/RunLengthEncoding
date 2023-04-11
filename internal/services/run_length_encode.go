9ef428f1e523394bed75b6eb3f1ab21662651229
Anton Trufanov <anton.t1984@gmail.com>
2023-04-11 11:17:46 +0300

    fix runLengthEncode

--- internal/services/run_length_encode.go
+++ internal/services/run_length_encode.go
@@ -5,42 +5,39 @@ import (
 	"strings"
 )
 
-// TODO
-// возвращать и обрабатывать ошибки
-// вынести контстанты
-// оптимизировать логику
-// пооборачивать в функции
 func runLengthEncode(msg []string) []string {
-	var firsrElement rune
-	count := 1
 	res := make([]string, len(msg))
 	sb := strings.Builder{}
+
 	for i := range msg {
+		var (
+			firsrElement rune
+			count        int
+		)
+
 		for _, elem := range msg[i] {
-			if firsrElement == 0 {
-				firsrElement = elem
-			} else if elem == firsrElement {
+			if elem == firsrElement {
 				count++
 				continue
-			} else if elem != firsrElement && count > 1 {
-				sb.WriteString(strconv.Itoa(count))
+			} else if elem != firsrElement && count > 0 {
+				sb.WriteString(strconv.Itoa(count + 1))
 				sb.WriteRune(firsrElement)
-				count = 1
-			} else if elem != firsrElement && count == 1 {
+				count = 0
+			} else if elem != firsrElement && firsrElement != 0 && count == 0 {
 				sb.WriteRune(firsrElement)
 			}
 			firsrElement = elem
 		}
-		if count > 1 {
-			sb.WriteString(strconv.Itoa(count))
+
+		if count > 0 {
+			sb.WriteString(strconv.Itoa(count + 1))
 			sb.WriteRune(firsrElement)
-			count = 1
 		} else if firsrElement == 0 {
 			sb.WriteString("")
 		} else {
 			sb.WriteRune(firsrElement)
 		}
-		firsrElement = 0
+
 		res[i] = sb.String()
 		sb.Reset()
 	}
